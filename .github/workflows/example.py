662c4f77cebd5646a81e6b395fadcd2108979de2
KPC6796 <54300384+KPC6796@users.noreply.github.com>
2022-08-17 23:20:13 +0530

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -1,5 +1,12 @@
+import os
+
 def main():
   print("Hello Kamakshi")
+  
+  token = os.environ.get('PYTHON_ACTION_MANUAL')
+  if not token:
+    raise RunTimeError('PYTHON_ACTION_MANUAL env var is not set')
+  print('We found our env var')
   
 if __name__ == '__main__':
   main()
